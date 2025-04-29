10460d1bd4f9b90b37c48dda7b5007111964b3c2
RobertoUlloa <roberto.ulloa@udla.edu.ec>
2025-04-29 08:41:08 -0500

    Crear los modelos cliente y reserva

---
+++ PruegaProgreso1UlloaR/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PruegaProgreso1UlloaR.Models
+{
+    public class Cliente
+    {
+        [Key]
+        [MaxLength(10)]
+        public int Identificacion { get; set; }
+        
+        public int Edad { get; set; }
+
+        [MaxLength(100)]
+        public string NombreApellido { get; set; }
+
+        public DateTime FechaEntrada { get; set; } = DateTime.Now; // Se asigna la fecha actual por defecto
+
+        public DateTime? FechaSalida { get; set; } // Se permite que sea nullable para que el usuario la asigne despuess
+
+
+    }
+}
---
+++ PruegaProgreso1UlloaR/Models/Reserva.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PruegaProgreso1UlloaR.Models
+{
+    public class Reserva
+    {
+        [Key]
+        public int IdReserva { get; set; }
+
+        public double Pagar { get; set; }
+        public string ClienteIdentificacion { get; set; }
+
+        [ForeignKey ("ClienteIdentificacion")]
+        public Cliente? Cliente { get; set; }
+
+    }
+}
