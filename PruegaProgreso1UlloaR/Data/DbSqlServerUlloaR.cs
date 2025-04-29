631a6991d6d25f08604110d1625038a62cd05931
RobertoUlloa <roberto.ulloa@udla.edu.ec>
2025-04-29 08:54:51 -0500

    Se crea un controlador para cliente

---
+++ PruegaProgreso1UlloaR/Data/DbSqlServerUlloaR.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PruegaProgreso1UlloaR.Models;
+
+    public class DbSqlServerUlloaR : DbContext
+    {
+        public DbSqlServerUlloaR (DbContextOptions<DbSqlServerUlloaR> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PruegaProgreso1UlloaR.Models.Cliente> Cliente { get; set; } = default!;
+    }
